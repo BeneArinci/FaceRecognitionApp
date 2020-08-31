7719f73d2f5a329145299bb2e1866e350d614f63
Bene Arinci <benedetta.arinci@gmail.com>
2020-08-31 17:58:42 +0100

    Updated what navbar is returning based on weather the user is signed in or not

--- src/App.js
+++ src/App.js
@@ -34,7 +34,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -72,7 +73,13 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})}
     this.setState({route: route})
+
+    console.log(this.state.isSignedIn)
   }
 
   render() {
@@ -80,7 +87,7 @@ class App extends Component {
       <div className="App">
         <Particles className='particles'
                 params={particlesOptions} />
-        <Navigation onRouteChange = {this.onRouteChange}/>
+        <Navigation onRouteChange = {this.onRouteChange} isSignedIn = {this.state.isSignedIn}/>
         
         { this.state.route === 'home' 
         ? <div> <Logo />
